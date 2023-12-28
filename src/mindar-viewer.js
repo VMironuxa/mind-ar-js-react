95c31603e365775a960db726b0ee9ff365d8630d
VMironuxa <116167552+VMironuxa@users.noreply.github.com>
2023-12-28 17:48:31 +0300

    fix

--- src/mindar-viewer.js
+++ src/mindar-viewer.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from 'react';
-import 'aframe';
-import 'mind-ar/dist/mindar-image-aframe.prod.js';
+import React, { useEffect, useRef } from "react";
+import "aframe";
+import "mind-ar/dist/mindar-image-aframe.prod.js";
 
 export default () => {
   const sceneRef = useRef(null);
@@ -8,27 +8,55 @@ export default () => {
   useEffect(() => {
     const sceneEl = sceneRef.current;
     const arSystem = sceneEl.systems["mindar-image-system"];
-    sceneEl.addEventListener('renderstart', () => {
-      arSystem.start(); // start AR 
+    sceneEl.addEventListener("renderstart", () => {
+      arSystem.start(); // start AR
     });
     return () => {
       arSystem.stop();
-    }
+    };
   }, []);
 
   return (
-    <a-scene ref={sceneRef} mindar-image="imageTargetSrc: https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/card.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;" color-space="sRGB" embedded renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+    <a-scene
+      ref={sceneRef}
+      mindar-image="imageTargetSrc:https://storage.yandexcloud.net/zrenie-dev-bucket/%D0%A2%D0%B5%D1%81%D1%82%D1%8B%20%D0%B4%D0%BB%D1%8F%20%D0%B4%D0%B5%D0%BC%D0%BE/targets.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
+      color-space="sRGB"
+      embedded
+      renderer="colorManagement: true"
+      vr-mode-ui="enabled: false"
+      device-orientation-permission-ui="enabled: false"
+    >
       <a-assets>
-        <img id="card" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/card.png" />
-        <a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
+        <img
+          id="card"
+          src="https://storage.yandexcloud.net/zrenie-dev-bucket/%D0%A2%D0%B5%D1%81%D1%82%D1%8B%20%D0%B4%D0%BB%D1%8F%20%D0%B4%D0%B5%D0%BC%D0%BE/%D0%BE%D1%82%D0%BA%D1%80%D1%8B%D1%82%D0%BA%D0%B0.png"
+        />
+        <a-asset-item
+          id="avatarModel"
+          rotation="0 180 0"
+          position="0 0 0"
+          scale="0.01 0.01 0.01"
+          src="https://storage.yandexcloud.net/zrenie-dev-bucket/%D0%A2%D0%B5%D1%81%D1%82%D1%8B%20%D0%B4%D0%BB%D1%8F%20%D0%B4%D0%B5%D0%BC%D0%BE/3d/scene.gltf"
+        ></a-asset-item>
       </a-assets>
 
-      <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+      <a-camera position=" 0 0 0" look-controls="enabled: false"></a-camera>
 
       <a-entity mindar-image-target="targetIndex: 0">
-        <a-plane src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0"></a-plane>
-        <a-gltf-model rotation="0 0 0 " position="0 0 0.1" scale="0.005 0.005 0.005" src="#avatarModel" animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"></a-gltf-model>
+        <a-plane
+          src="#card"
+          position="0 0 0"
+          height="1"
+          width="1"
+          rotation="0 0 0"
+        ></a-plane>
+        <a-gltf-model
+          src="#avatarModel"
+          rotation="90 0 0 "
+          // scale="10 10 10"
+          animation="property: position; to: 0.1; dur: 1000; loop: true; dir: alternate"
+        ></a-gltf-model>
       </a-entity>
     </a-scene>
-  )
-}
+  );
+};
